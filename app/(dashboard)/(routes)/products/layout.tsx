cf573f65fbe6478e1bd47f5e411c1d7c01ccb0a5
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-09-29 20:21:39 +0200

    layout for productpage

---
+++ app/(dashboard)/(routes)/products/layout.tsx
@@ -0,0 +1,13 @@
+export default function ProductsLayout({
+    children, // will be a page or nested layout
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <section className={"w-full"}>
+        {/* Include shared UI here e.g. a header or sidebar */}
+   
+        {children}
+      </section>
+    )
+  }
