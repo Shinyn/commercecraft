01c240ed4b17d7a6d5178da8195be1947a8f4f99
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-09-29 20:24:30 +0200

    products page with datatable component

---
+++ app/(dashboard)/(routes)/products/page.tsx
@@ -0,0 +1,35 @@
+import {DataTable} from "../../../../components/data-table"
+import { Product, columns } from "./columns"
+
+
+async function getData(): Promise<Product[]> {
+    // Fetch data from your API here.
+    return [
+    //   {
+    //     id: "728ed52f",
+    //     title: "Milk",
+    //     description: "bla bla bla",
+    //     price: 100,
+    //   },
+      // ...
+    ]
+  }
+
+export default async function Page() {
+
+const data = await getData()
+
+    return (
+        <div className={"m-9"} >
+            <div className={"flex flex-row justify-between"}>
+                <h1 className=" text-5xl ">Products</h1>
+                <button className="bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+                    Add Product
+                </button>
+            </div>
+            <div className="container mx-auto py-10">
+                <DataTable columns={columns} data={data} />
+            </div>
+        </div>
+    )
+}
