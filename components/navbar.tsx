918bb1fdc8ad08dac2c1ce1e320088cf7cede376
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-09-29 20:20:01 +0200

    self-made navbar but all links go to products

---
+++ components/navbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+
+
+export default function Navbar() {
+  return (
+
+    <div className= {" p-2 h-screen bg-blue-400 "}>
+        <h1 className= {"m-6 text-5xl"}>Dashboard</h1>
+        <ul className={"flex-col space-y-8 m-4  text-3xl"}>
+            <li className={"hover:text-gray-400"}><Link href="/products">Banner</Link></li>
+            <li className={"hover:text-gray-400"}><Link href="/products">Products</Link></li>
+            <li className={"m-1hover:text-gray-400 text-2xl"}><Link href="/products">Categories</Link></li>
+            <li className={"m-1 hover:text-gray-400 text-2xl"}><Link href="/products">Sizes</Link></li>
+            <li className={"m-1 hover:text-gray-400 text-2xl"}><Link href="/products">Colors</Link></li>
+            <li className={"hover:text-gray-400"}><Link href="/products">Customers</Link></li>
+            <li className={"hover:text-gray-400"}><Link href="/products">Orders</Link></li>
+        </ul>
+    </div>
+    
+
+  )
+}
