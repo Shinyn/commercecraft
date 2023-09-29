5704ee61fe5f8a1d43142a7c1d037c0868ad3a5e
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-09-29 20:21:19 +0200

    Navbar in layout for dashboard-routes

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import Navbar from "../../components/navbar"
+
+export default function DashboardLayout({
+    children, // will be a page or nested layout
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+        
+      <div className={"flex flex-row justify-start"}>
+        {<Navbar/>}
+        
+        {children}
+      </div>
+    )
+  }
