140ab2158c55b19706e8b5be046eb4e4961ef4c9
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-10-03 09:53:10 +0200

    useEffect to fetch categories and maps in tabl etc

--- app/(dashboard)/(routes)/categories/page.tsx
+++ app/(dashboard)/(routes)/categories/page.tsx
@@ -1,98 +1,64 @@
 
+'use client'
+ import { useNameStore } from '@/components/route-components/dasboard/categories/zustand/zustandstate';
+ import { DataTable } from '@/components/data-table';
 
-//Contains fetch to GET alla products from api/products
 
-// 'use client'
+ import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+  } from "@/components/ui/popover"
 
-// import axios from 'axios'
-// import { create } from 'zustand';
-
-// type State = {
-//     name: string,
-//     items:Map<string, string>;
-//   }
-  
-//   type Action = {
-//     updateName: (name: State['name']) => void
-//     updateItems: (items: State['items']) => void
-//   }
-
-//   const useItemStore = create<State & Action>((set) => ({
-//     name: '',
-//     updateName: (name: string) => set(() => ({ name: name })),
-//     items: new Map<string, string>(),
-//     updateItems: (items: Map<string, string>) => set(() => ({ items: items }))
-//   }))
+ import CategoryForm from '@/components/route-components/dasboard/categories/categoryform';
+ import { columns } from '@/components/route-components/dasboard/categories/columns';
+import axios from 'axios'
+import { useEffect} from 'react'
 
 
 
-  
-
-// export default function Page() {
-//     const Itemname = useItemStore((state) => state.name)
-//     const updateName = useItemStore((state) => state.updateName)
-//     const items = useItemStore((state) => state.items)
-//     const updateItems = useItemStore((state) => state.updateItems)
-//     const itemsitr = items.entries()
-
-//     function getProducts(e:React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-//         e.preventDefault()
-//         console.log('button clicked')
-//         axios.get('/api/products', {})
-//             .then(function (response) {
-//                 console.log(response);
-//             })
-//             .catch(function (error) {
-//                 console.log(error);
-//             });
-//     }
-
-//     function getCategories(e:React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-//         e.preventDefault()
-//         console.log('button clicked')
-//         axios.get('/api/categories', {})
-//             .then(function (response) {
-//                 response.data.forEach((item: { id: string; name: string; }) => {
-//                     items.set(item.id, item.name)
-//                 });
-//             })
-//             .catch(function (error) {
-//                 console.log(error);
-//             });
-//     }
-
-//     function postCategory(e:React.FormEvent<HTMLFormElement>) {
-//         e.preventDefault()
-//         console.log('button clicked')
-//         axios.post('/api/categories', {
-//             name: Itemname
-//     })
-//             .then(function (response) {
-//                 console.log(response);
-//             })
-//             .catch(function (error) {
-//                 console.log(error);
-//             });
-//     }
 
 
-  
 
-//     return (
-//         <div>
-//             <h1>Products</h1>
-//             <button onClick={getCategories}>Get Categories</button>
-//             <form onSubmit={postCategory}>
-//                 <label>New Category</label>
-//                 <input type="text" name="name" value = {Itemname} onChange={(e) => updateName(e.currentTarget.value)}/>
-//                 <button>Create</button>
-//             </form>
-
-//         {Array.from(itemsitr).map(([key, value]) => (
-//                 <h2 key={key}>{value}</h2>
-//         ))}
+export default function Page() {
+    const categories = useNameStore((state) => state.categories)
+    const updateCategories = useNameStore((state) => state.updateCategories)
+    const state = useNameStore((state) => state.state)
+    const updateState = useNameStore((state) => state.updateState)
 
 
-//         </div>
-//     )
-// }
+       
+      
+
+    useEffect(() => {
+        axios.get('/api/categories', {})
+            .then(function (response) {
+                updateCategories(response.data)
+                    updateState(true)
+            
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }, [])
+
+
+    return (
+        <>
+            <div className={"flex flex-row justify-end "}>
+                <div className={"text-center text-2xl m-9 rounded bg-blue-400 w-9"}>
+                <Popover> 
+                 <PopoverTrigger> + </PopoverTrigger>
+                <PopoverContent><CategoryForm/></PopoverContent>
+                </Popover>
+                </div>
+            </div>
+        <div>
+            <div className="container mx-auto py-10">
+                <DataTable columns={columns} data={categories} />
+            </div>
+
+        </div>
+    </>
+    )
+}
