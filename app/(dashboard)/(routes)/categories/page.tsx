9bf72a448a625b216f679add351d6e14eae34809
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-10-03 15:07:13 +0200

    flyttat alla mappjevlar

--- app/(dashboard)/(routes)/categories/page.tsx
+++ app/(dashboard)/(routes)/categories/page.tsx
@@ -1,58 +1,56 @@
+"use client";
+import { useNameStore } from "@/components/dashboard/categories/zustand/zustandstate";
+import { DataTable } from "@/components/data-table";
 
-'use client'
- import { useNameStore } from '@/components/route-components/dasboard/categories/zustand/zustandstate';
- import { DataTable } from '@/components/data-table';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
-
- import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-  } from "@/components/ui/popover"
-
- import CategoryForm from '@/components/route-components/dasboard/categories/categoryform';
- import { columns } from '@/components/route-components/dasboard/categories/columns';
- import axios from 'axios'
- import { useEffect} from 'react'
+import CategoryForm from "@/components/dashboard/categories/categoryform";
+import { columns } from "@/components/dashboard/categories/columns";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function Page() {
+  //State for the categories
+  const categories = useNameStore((state) => state.categories);
+  const updateCategories = useNameStore((state) => state.updateCategories);
+  const state = useNameStore((state) => state.state);
+  const updateState = useNameStore((state) => state.updateState);
 
-    //State for the categories
-    const categories = useNameStore((state) => state.categories)
-    const updateCategories = useNameStore((state) => state.updateCategories)
-    const state = useNameStore((state) => state.state)
-    const updateState = useNameStore((state) => state.updateState)
+  //Get the categories from the database
+  useEffect(() => {
+    axios
+      .get("/api/categories", {})
+      .then(function (response) {
+        updateCategories(response.data);
+        updateState(true);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 
-    //Get the categories from the database
-    useEffect(() => {
-        axios.get('/api/categories', {})
-            .then(function (response) {
-                updateCategories(response.data)
-                    updateState(true)
-            
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }, [])
-
-//Render the page, pop up for adding a category and the table with the categories
-    return (
-        <>
-            <div className={"flex flex-row justify-end "}>
-                <div className={"text-center text-2xl m-9 rounded bg-blue-400 w-9"}>
-                <Popover> 
-                 <PopoverTrigger> + </PopoverTrigger>
-                <PopoverContent><CategoryForm/></PopoverContent>
-                </Popover>
-                </div>
-            </div>
-        <div>
-            <div className="container mx-auto py-10">
-                <DataTable columns={columns} data={categories} />
-            </div>
-
+  //Render the page, pop up for adding a category and the table with the categories
+  return (
+    <>
+      <div className={"flex flex-row justify-end "}>
+        <div className={"text-center text-2xl m-9 rounded bg-blue-400 w-9"}>
+          <Popover>
+            <PopoverTrigger> + </PopoverTrigger>
+            <PopoverContent>
+              <CategoryForm />
+            </PopoverContent>
+          </Popover>
         </div>
+      </div>
+      <div>
+        <div className="container mx-auto py-10">
+          <DataTable columns={columns} data={categories} />
+        </div>
+      </div>
     </>
-    )
+  );
 }
--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,42 +1,40 @@
 import { NextResponse } from "next/server";
 import prismadb from "@/lib/db";
-import { Category } from "@/components/route-components/dasboard/categories/categories";
+import { Category } from "@/components/dashboard/categories/categories";
 
 // Types
 
 // Route handlers for /api/categories
-export async function POST(
-    req: Request,
-)
-{
-    try {
-        const body = await req.json()
-        const {title}: Category = body
-        const newCategory = await prismadb.category.create({
-            data: {
-                title,
-            },
-        })
-        return NextResponse.json(newCategory, { status: 201 })
-
-    } catch (error) {
-        console.log("api/categories/POST", error)
-        return new NextResponse("Something went wrong when trying to save your category", { status: 500 })
-    }
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { title }: Category = body;
+    const newCategory = await prismadb.category.create({
+      data: {
+        title,
+      },
+    });
+    return NextResponse.json(newCategory, { status: 201 });
+  } catch (error) {
+    console.log("api/categories/POST", error);
+    return new NextResponse(
+      "Something went wrong when trying to save your category",
+      { status: 500 }
+    );
+  }
 }
 
-export async function GET(
-    req: Request,
-)
-{
-    try {
-        const Categories = await prismadb.category.findMany()
-        return NextResponse.json(Categories)
-
-    } catch (error) {
-        console.log("api/categories/GET", error)
-        return new NextResponse("Ooops, something went wrong when getting the categories", { status: 500 })
-    }
+export async function GET(req: Request) {
+  try {
+    const Categories = await prismadb.category.findMany();
+    return NextResponse.json(Categories);
+  } catch (error) {
+    console.log("api/categories/GET", error);
+    return new NextResponse(
+      "Ooops, something went wrong when getting the categories",
+      { status: 500 }
+    );
+  }
 }
 
 // TODO Route handlers for /api/categories/[id]
