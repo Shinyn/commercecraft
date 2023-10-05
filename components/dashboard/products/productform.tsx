226ae30f0997e3070a3e25e23ee58647c6911d79
Shinyn <mattias.alm@hotmail.com>
2023-10-05 16:30:49 +0200

    Fixed number issues, added image input with zod

--- components/dashboard/products/productform.tsx
+++ components/dashboard/products/productform.tsx
@@ -1,106 +1,99 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
-import axios from "axios";
-import { Button } from "@/components/ui/button";
-import { useForm } from "react-hook-form";
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+import axios from 'axios';
+import { Button } from '@/components/ui/button';
+import { useForm } from 'react-hook-form';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 
 export default function ProductForm() {
-    const productSchema = z.object({
-        id: z.string().min(2, { message: "ID must be at least 2 characters long" }),
-        title: z.string().min(2, { message: "Title must be at least 2 characters long" }),
-        description: z.string().min(5, { message: "Description must be at least 5 characters long" }),
-        price: z.number().min(0, { message: "Price must be a positive number" }),
-    });
+  const productSchema = z.object({
+    title: z.string().min(2, { message: 'Title must be at least 2 characters long' }),
+    description: z.string().min(5, { message: 'Description must be at least 5 characters long' }),
+    price: z.number().min(0, { message: 'Price must be a positive number' }).max(Infinity),
+    image: z.string().url({ message: 'Need URL' }),
+  });
 
-    const form = useForm<z.infer<typeof productSchema>>({
-        resolver: zodResolver(productSchema),
-        defaultValues: {
-            id: "",
-            title: "",
-            description: "",
-            price: 0,
-        },
-    });
+  const form = useForm<z.infer<typeof productSchema>>({
+    resolver: zodResolver(productSchema),
+    defaultValues: {
+      title: '',
+      description: '',
+      price: 1,
+      image: '',
+    },
+  });
 
-    function onSubmitting(values: z.infer<typeof productSchema>) {
-        axios.post('/api/products', values)
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
+  function onSubmitting(values: z.infer<typeof productSchema>) {
+    axios
+      .post('/api/products', values)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-    return (
-        <div className={"m-9"}>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmitting)} className="space-y-8">
-                    <FormField
-                        control={form.control}
-                        name="id"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Product ID</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Product ID" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="title"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Product Title</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Product Title" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="description"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Product Description</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Product Description" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="price"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Product Price</FormLabel>
-                                <FormControl>
-                                    <Input type="number" placeholder="Product Price" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <Button type="submit">Submit</Button>
-                </form>
-            </Form>
-        </div>
-    );
+  return (
+    <div className={'m-9'}>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmitting)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Product Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Product Title" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Product Description</FormLabel>
+                <FormControl>
+                  <Input placeholder="Product Description" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Product Price</FormLabel>
+                <FormControl>
+                  <Input type="number" min={1} {...field} onChange={(event) => field.onChange(+event.target.value)} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="image"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Image URL</FormLabel>
+                <FormControl>
+                  <Input placeholder="Image URL here" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
 }
