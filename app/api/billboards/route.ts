bc54952e81a5289056160118023ef7da113d589f
TereseBo <terese.boderus@gmail.com>
2023-10-06 11:46:09 +0200

    PUT handler added

--- app/api/billboards/route.ts
+++ app/api/billboards/route.ts
@@ -11,7 +11,7 @@ export async function POST(
     try {
         const body = await req.json()
         const { text, image, active }: Billboard = body.values
-        let newBillboard=undefined
+        let newBillboard = undefined
         //Remove activation from any previous billboards.
         if (active) {
             const updated = await prismadb.billboard.updateMany({
@@ -19,7 +19,7 @@ export async function POST(
                 data: { active: 0 },
             })
                 .then(async function (response) {
-                   newBillboard = await prismadb.billboard.create({
+                    newBillboard = await prismadb.billboard.create({
                         data: {
                             text,
                             image,
@@ -35,7 +35,7 @@ export async function POST(
                 data: {
                     text,
                     image,
-                    active:0
+                    active: 0
                 },
             })
 
@@ -62,4 +62,55 @@ export async function GET(
         return new NextResponse("Ooops, something went wrong when getting the billboards", { status: 500 })
     }
 }
+
+export async function PUT(
+    req: Request,
+    //TODO: Add params for extracting store when stores are implemented in dashboard
+) {
+    try {
+        const body = await req.json()
+        const { id, text, image, active }: Billboard = body.values
+        let newBillboard = undefined
+        
+        if (!id) {
+            return NextResponse.json("Billboard id is missing from the request", { status: 400 })
+        }
+        if (active) {//Remove activation from any previous billboards if needed
+            const updated = await prismadb.billboard.updateMany({
+                where: { active: 1 },
+                data: { active: 0 },
+            })
+                .then(async function (response) {//update the billboard
+                    newBillboard = await prismadb.billboard.update({
+                        where: { id },
+                        data: {
+                            text,
+                            image,
+                            active: 1
+                        },
+                    })
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        } else {
+            newBillboard = await prismadb.billboard.update({
+                where: { id },
+                data: {
+                    text,
+                    image,
+                    active: 0
+                },
+            })
+        }
+
+        return NextResponse.json(newBillboard, { status: 200 })
+
+    } catch (error) {
+        console.log("api/billboards/POST", error)
+        return new NextResponse("Something went wrong when trying to update your billboard", { status: 500 })
+    }
+}
+
+
 
