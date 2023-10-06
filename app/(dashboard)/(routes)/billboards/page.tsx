6dd6e510953df46610c8b2b8a1b370de7d977675
TereseBo <terese.boderus@gmail.com>
2023-10-06 11:45:30 +0200

    Frontend components for Billboard handeling

---
+++ app/(dashboard)/(routes)/billboards/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { DashboardPopover } from "@/components/DashboardPopover"
+import { DataTable } from "@/components/data-table";
+import { columns } from "@/components/dashboard/billboards/columns";
+import { useEffect } from "react";
+import { PostForm } from "@/components/dashboard/billboards/PostForm"
+import axios from "axios"
+import { BillboardState } from "@/components/dashboard/billboards/state";
+
+export default function Page() {
+  //states for keeping track of billboards in dashboard front
+  const billboards = BillboardState((state) => state.billboards);
+  const updateBillboards = BillboardState((state) => state.updateBillboards);
+
+  useEffect(() => {
+    axios
+      .get("/api/billboards", {})
+      .then(function (response) {
+        console.log(response.data)
+        updateBillboards(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <>
+      <DashboardPopover>
+        <PostForm />
+      </DashboardPopover>
+      <div>
+        <div className="container mx-auto py-10">
+          <DataTable columns={columns} data={billboards} />
+        </div>
+      </div>
+    </>
+  );
+}
