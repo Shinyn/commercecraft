18e37ce54dfaa5dd5f666cf7ed41274da67fdb19
BeccaFi <rebecca.fischer.dev@gmail.com>
2023-10-09 14:57:34 +0200

    Added Clerk according to install docs

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,15 @@
 //Layout för hela dashboarden
+import Navbar from "@/components/navbar";
 
 export default function DashboardLayout({
-    children, // will be a page or nested layout
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-        
-      <div className={"w-full"}>
-        {children}
-      </div>
-    )
-  }
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={"flex flex-row justify-start"}>
+      {<Navbar />}
+      {children}
+    </div>
+  );
+}
