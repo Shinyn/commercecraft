cdfcc54064e3ee44094a0b9395373d843f1616ac
BeccaFi <rebecca.fischer.dev@gmail.com>
2023-10-10 14:51:46 +0200

    Delete store api

---
+++ components/stores/deleteStore.tsx
@@ -0,0 +1,27 @@
+import axios, { isAxiosError } from "axios";
+import toast from "react-hot-toast";
+import { useAuth } from "@clerk/nextjs";
+
+export function MyDelete(params: { ID: string | undefined }) {
+  const { userId } = useAuth();
+  return (
+    <div
+      onClick={(e) => {
+        const response = axios
+          .delete(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/stores/${userId}/${params.ID}`,
+            {}
+          )
+          .then(function (response) {
+            window.location.reload();
+            return response.data;
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }}
+    >
+      Delete
+    </div>
+  );
+}
