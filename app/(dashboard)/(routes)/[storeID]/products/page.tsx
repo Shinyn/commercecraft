6dcc14e666f7dd75a0229abae38b5fa2f48f01f6
BeccaFi <rebecca.fischer.dev@gmail.com>
2023-10-10 13:08:16 +0200

    Merge conflict

--- app/(dashboard)/(routes)/[storeID]/products/page.tsx
+++ app/(dashboard)/(routes)/[storeID]/products/page.tsx
@@ -2,11 +2,7 @@
 import { useProductStore } from "@/components/stores/dashboard/products/zustand/zustandstate";
 import { DataTable } from "@/components/data-table";
 
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+import {DashboardPopover} from  "@/components/DashboardPopover"
 
 import ProductForm from "@/components/stores/dashboard/products/productform";
 import { columns } from "@/components/stores/dashboard/products/columns";
@@ -33,16 +29,9 @@ export default function Page() {
   // Rendera sidan, popup för att lägga till en produkt och tabellen med produkter
   return (
     <>
-      <div className={"flex flex-row justify-end "}>
-        <div className={"text-center text-2xl m-9 rounded bg-blue-400 w-9"}>
-          <Popover>
-            <PopoverTrigger>+</PopoverTrigger>
-            <PopoverContent>
-              <ProductForm />
-            </PopoverContent>
-          </Popover>
-        </div>
-      </div>
+ <DashboardPopover>
+        <ProductForm />
+      </DashboardPopover>
       <div>
         <div className="container mx-auto py-10">
           <DataTable columns={columns} data={products} />
