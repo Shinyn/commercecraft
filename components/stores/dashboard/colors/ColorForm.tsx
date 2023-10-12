d31960014100df408036924daeef39527a2336fb
arapg <hello@arpg.dev>
2023-10-12 18:05:58 +0200

    added form for colors

---
+++ components/stores/dashboard/colors/ColorForm.tsx
@@ -0,0 +1,80 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import toast, { Toaster } from "react-hot-toast";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useParams } from "next/navigation";
+
+//Form for adding a new category
+export default function ColorForm() {
+  const params = useParams();
+  //Form validation
+  const formSchema = z.object({
+    name: z.string().max(10),
+  });
+  //Form hook
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  //Submit function
+  function onSubmitting(values: z.infer<typeof formSchema>) {
+    axios
+      .post(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/${params.storeID}/colors`,
+        {
+          title: values.name,
+        }
+      )
+      .then(function (response) {
+        console.log(response);
+        toast.success("Color added successfully!");
+        setInterval(() => {
+          window.location.reload();
+        }, 3000);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  //Render the form
+  return (
+    <div className={"m-9"}>
+      <Toaster />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmitting)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>New Size</FormLabel>
+                <FormControl>
+                  <Input placeholder="color" {...field} />
+                </FormControl>
+                <FormDescription>Type in the color here.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
