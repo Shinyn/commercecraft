febe8cab1bbe9e05e339c45ee3a7ff6d5235f2aa
arapg <hello@arpg.dev>
2023-10-12 18:09:04 +0200

    page layout and page in dashboard

---
+++ app/(dashboard)/(routes)/[storeID]/colors/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useNameStore } from "@/components/stores/dashboard/colors/zustand/zustandstate";
+import { DataTable } from "@/components/data-table";
+import { DashboardPopover } from "@/components/DashboardPopover";
+
+import ColorForm from "@/components/stores/dashboard/colors/ColorForm";
+import { columns } from "@/components/stores/dashboard/colors/columns";
+import axios from "axios";
+import { useEffect } from "react";
+import { useParams } from "next/navigation";
+
+export default function Page() {
+  //State for the colors
+  const colors = useNameStore((state) => state.colors);
+  const updateColors = useNameStore((state) => state.updateColors);
+  const state = useNameStore((state) => state.state);
+  const updateState = useNameStore((state) => state.updateState);
+  const params = useParams();
+
+  //Get the colors from the database
+  useEffect(() => {
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/${params.storeID}/colors`,
+        {}
+      )
+      .then(function (response) {
+        updateColors(response.data);
+        updateState(true);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  //Render the page, pop up for adding a color and the table with the colors
+  return (
+    <div className={"flex flex-col"}>
+      <DashboardPopover>
+        <ColorForm />
+      </DashboardPopover>
+
+      <div>
+        <div className="container mx-auto py-10">
+          <DataTable columns={columns} data={colors} />
+        </div>
+      </div>
+    </div>
+  );
+}
