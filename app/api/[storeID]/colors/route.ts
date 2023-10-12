2323700f6c2a40ebbc51808b3a9570c4e15571d7
arapg <hello@arpg.dev>
2023-10-12 18:09:14 +0200

    added color routes

---
+++ app/api/[storeID]/colors/route.ts
@@ -0,0 +1,67 @@
+import { NextResponse } from "next/server";
+import prismadb from "@/lib/db";
+import { Color } from "@/components/stores/dashboard/colors/colors";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeID: string } }
+) {
+  try {
+    const body = await req.json();
+    body.storeId = params.storeID;
+    const { title, storeId }: Color = body;
+    const newColor = await prismadb.color.create({
+      data: {
+        storeId,
+        title,
+      },
+    });
+    return NextResponse.json(newColor, { status: 201 });
+  } catch (error) {
+    console.log("api/colors/POST", error);
+    return new NextResponse(
+      "Something went wrong when trying to save your color",
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeID: string } }
+) {
+  try {
+    const Colors = await prismadb.color.findMany({
+      where: { storeId: params.storeID },
+    });
+    return NextResponse.json(Colors);
+  } catch (error) {
+    console.log("api/colors/GET", error);
+    return new NextResponse(
+      "Ooops, something went wrong when getting the colors",
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(req: Request) {
+  try {
+    const body = await req.json();
+    const { id, title } = body;
+    const updatedColor = await prismadb.color.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+      },
+    });
+    return NextResponse.json(updatedColor, { status: 200 });
+  } catch (error) {
+    console.log("api/colors/PATCH", error);
+    return new NextResponse(
+      "Ooops, something went wrong when updating the color",
+      { status: 500 }
+    );
+  }
+}
