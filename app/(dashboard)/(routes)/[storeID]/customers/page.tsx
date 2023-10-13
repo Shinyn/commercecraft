f9f628186c3c3c00410f69c569f27cf35045611f
TereseBo <terese.boderus@gmail.com>
2023-10-13 15:42:45 +0200

    Added page templates

---
+++ app/(dashboard)/(routes)/[storeID]/customers/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useEffect } from "react";
+import axios from "axios";
+import { useParams } from "next/navigation";
+
+export default function Page() {
+  //states for keeping track of billboards in dashboard front
+  const params = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`/api/${params.storeID}/customers`, {})
+      .then(function (response) {
+        console.log(response.data)
+
+    })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <div className={"flex flex-col"}>
+      <div>
+        <div className="container mx-auto py-10">
+            Customers-page
+        </div>
+      </div>
+    </div>
+  );
+}
