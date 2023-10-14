d53806785bc12397a960274c69f8ba4409476283
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-10-14 15:32:19 +0200

    email instead of e_mail

--- components/stores/dashboard/customers/columns.tsx
+++ components/stores/dashboard/customers/columns.tsx
@@ -123,36 +123,36 @@ export const columns: ColumnDef<Customer>[] = [
   {
     id: "actions",
     cell: ({ row }) => {
-
       const category = row.original;
       function copyMail() {
         if (category.id === undefined) return "no id";
-        else return category.e_mail.toString();
+        else return category.email.toString();
       }
 
       return (
         <>
           {/* //Sheet to cover background avd display EditForm*/}
 
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="h-8 w-8 p-0">
-                  <span className="sr-only">Open menu</span>
-                  <MoreHorizontal className="h-4 w-4" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                  <DropdownMenuItem
-                    onClick={() =>navigator.clipboard.writeText(copyMail())}>
-                    Copy e-mail
-                  </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <ContactLinkCustomer e_mail={row.original.e_mail} />
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => navigator.clipboard.writeText(copyMail())}
+              >
+                Copy e-mail
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <ContactLinkCustomer e_mail={row.original.email} />
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </>
       );
     },
