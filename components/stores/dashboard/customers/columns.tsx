960430a5afb17cacfc89fbd7d2998c4e81593d70
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-10-14 15:33:21 +0200

    Email instead of City

--- components/stores/dashboard/customers/columns.tsx
+++ components/stores/dashboard/customers/columns.tsx
@@ -113,7 +113,7 @@ export const columns: ColumnDef<Customer>[] = [
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-          City
+          Email
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       );
