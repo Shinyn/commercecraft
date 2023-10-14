c1697c9d03c2831d68f33856d0eff9251def15ef
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-10-14 15:31:32 +0200

    Fixade page

--- app/(dashboard)/(routes)/[storeID]/customers/page.tsx
+++ app/(dashboard)/(routes)/[storeID]/customers/page.tsx
@@ -1,8 +1,8 @@
-'use client'
+"use client";
 import { useEffect } from "react";
 import axios from "axios";
 import { useParams } from "next/navigation";
-import {columns} from "@/components/stores/dashboard/customers/columns"
+import { columns } from "@/components/stores/dashboard/customers/columns";
 import { DashboardPopover } from "@/components/DashboardPopover";
 import { DataTable } from "@/components/data-table";
 export default function Page() {
@@ -11,57 +11,55 @@ export default function Page() {
 
   //Sample data until state is implemented
   //TODO:Add state for holding customers
-const customers=[
-  {
-    id: "12323543253",
-    storeId: "298f4164-0d99-4b2d-a054-4fe4fac7e498",
-    firstName: "John",
-    lastName: "Doe",
-    street: "BabyDollRoad",
-    zipCode: 11111,
-    city: "Barbietown",
-    e_mail: "john.doe@theuniverse.com",
-  },
-  {
-    id: "12323543253",
-    storeId: "298f4164-0d99-4b2d-a054-4fe4fac7e498",
-    firstName: "John",
-    lastName: "Doe",
-    street: "BabyDollRoad",
-    zipCode: 11111,
-    city: "Barbietown",
-    e_mail: "john.doe@theuniverse.com",
-  },
-  {
-    id: "12323543253",
-    storeId: "298f4164-0d99-4b2d-a054-4fe4fac7e498",
-    firstName: "John",
-    lastName: "Doe",
-    street: "BabyDollRoad",
-    zipCode: 11111,
-    city: "Barbietown",
-    e_mail: "john.doe@theuniverse.com",
-  }
-
-]
-
+  const customers = [
+    {
+      id: "12323543253",
+      storeId: "298f4164-0d99-4b2d-a054-4fe4fac7e498",
+      firstname: "John",
+      lastname: "Doe",
+      street: "BabyDollRoad",
+      zipCode: "11111",
+      city: "Barbietown",
+      email: "john.doe@theuniverse.com",
+      phone: "1234567890",
+    },
+    {
+      id: "12323543253",
+      storeId: "298f4164-0d99-4b2d-a054-4fe4fac7e498",
+      firstname: "John",
+      lastname: "Doe",
+      street: "BabyDollRoad",
+      zipCode: "11111",
+      city: "Barbietown",
+      email: "john.doe@theuniverse.com",
+      phone: "1234567890",
+    },
+    {
+      id: "12323543253",
+      storeId: "298f4164-0d99-4b2d-a054-4fe4fac7e498",
+      firstname: "John",
+      lastname: "Doe",
+      street: "BabyDollRoad",
+      zipCode: "11111",
+      city: "Barbietown",
+      email: "john.doe@theuniverse.com",
+      phone: "1234567890",
+    },
+  ];
 
   useEffect(() => {
     axios
       .get(`/api/${params.storeID}/customers`, {})
       .then(function (response) {
-        console.log(response.data)
-
-    })
+        console.log(response.data);
+      })
       .catch(function (error) {
         console.log(error);
       });
   }, []);
   return (
     <div className={"flex flex-col"}>
-      <DashboardPopover>
-        Not implemented yet
-      </DashboardPopover>
+      <DashboardPopover>Not implemented yet</DashboardPopover>
       <div>
         <div className="container mx-auto py-10">
           <DataTable columns={columns} data={customers} />
