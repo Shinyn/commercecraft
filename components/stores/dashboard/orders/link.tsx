021e2252d4462903e36726836eddecfc3f6711fe
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-10-15 23:33:19 +0200

    unbroke the broken

--- components/stores/dashboard/orders/link.tsx
+++ components/stores/dashboard/orders/link.tsx
@@ -1,10 +1,7 @@
-import React from "react";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
-const LinktoPrint = (props: string) => {
+export function LinktoPrint(props: string) {
   const params = useParams();
   return <Link href={`/${params.storeID}/orders/${props}`}></Link>;
-};
-
-export default LinktoPrint;
+}
