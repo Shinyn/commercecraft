fdbb5d786bed49afbd0d78273aadd88756a41762
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-10-15 23:34:12 +0200

    FORGOT THE PRINT

--- components/stores/dashboard/orders/link.tsx
+++ components/stores/dashboard/orders/link.tsx
@@ -3,5 +3,5 @@ import { useParams } from "next/navigation";
 
 export function LinktoPrint(props: string) {
   const params = useParams();
-  return <Link href={`/${params.storeID}/orders/${props}`}></Link>;
+  return <Link href={`/${params.storeID}/orders/${props}`}>Print here:</Link>;
 }
