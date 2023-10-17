476a355bb88c62c85c2f111215de94644d61ab5b
Shinyn <mattias.alm@hotmail.com>
2023-10-17 12:13:33 +0200

    User is to the right (wow)

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
-"use client";
-import { UserButton } from "@clerk/nextjs";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { useAuth } from "@clerk/nextjs";
-import StoreForm from "@/components/stores/newStoreForm";
-import { columns } from "../components/stores/columns";
-import { DataTable } from "@/components/data-table";
+'use client';
+import { UserButton } from '@clerk/nextjs';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useAuth } from '@clerk/nextjs';
+import StoreForm from '@/components/stores/newStoreForm';
+import { columns } from '../components/stores/columns';
+import { DataTable } from '@/components/data-table';
 
 export default function Home() {
   const [stores, setStores] = useState([]);
   const { userId } = useAuth();
+
   useEffect(() => {
     axios
       .get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/stores/${userId}`, {})
@@ -23,9 +24,10 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="">
-      <p>Under contruction</p>
-      <UserButton afterSignOutUrl="/" />
+    <main className="min-h-screen">
+      <div className="flex justify-end m-8">
+        <UserButton afterSignOutUrl="/" />
+      </div>
       <StoreForm />
       <DataTable columns={columns} data={stores} />
     </main>
