b969f363764550461159b10bff215f8c81066a74
Shinyn <mattias.alm@hotmail.com>
2023-10-17 16:07:56 +0200

    Fixed productform design

--- components/stores/dashboard/products/productform.tsx
+++ components/stores/dashboard/products/productform.tsx
@@ -1,83 +1,64 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
-import axios from "axios";
-import { Button } from "@/components/ui/button";
-import { useForm } from "react-hook-form";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Checkbox } from "@/components/ui/checkbox";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import SelectForAddProduct from "@/components/stores/dashboard/products/SelectForAddProduct";
-import { useParams } from "next/navigation";
-import toast from "react-hot-toast";
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+import axios from 'axios';
+import { Button } from '@/components/ui/button';
+import { useForm } from 'react-hook-form';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Checkbox } from '@/components/ui/checkbox';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import SelectForAddProduct from '@/components/stores/dashboard/products/SelectForAddProduct';
+import { useParams } from 'next/navigation';
+import toast from 'react-hot-toast';
 
 export default function ProductForm() {
   const params = useParams();
   const productSchema = z.object({
-    title: z
-      .string()
-      .min(2, { message: "Name must be at least 2 characters long" }),
+    title: z.string().min(2, { message: 'Name must be at least 2 characters long' }),
     description: z
       .string()
-      .min(2, { message: "Description must be at least 2 characters long" })
-      .max(255, { message: "Description must be less than 255 characters" }),
-    price: z
-      .number()
-      .min(0, { message: "Price must be a positive number" })
-      .max(Infinity),
-    image: z
-      .string()
-      .url({ message: "Need URL" })
-      .max(255, { message: "URL must be less than 255 characters" }),
+      .min(2, { message: 'Description must be at least 2 characters long' })
+      .max(255, { message: 'Description must be less than 255 characters' }),
+    price: z.number().min(0, { message: 'Price must be a positive number' }).max(Infinity),
+    image: z.string().url({ message: 'Need URL' }).max(255, { message: 'URL must be less than 255 characters' }),
     category: z.string(),
     color: z.string(),
     size: z.string(),
     manufacturer: z.string().min(2, {
-      message: "Manufacturer must be at least 2 characters long",
+      message: 'Manufacturer must be at least 2 characters long',
     }),
     isarchived: z.boolean(),
     isfeatured: z.boolean(),
-    ingredients: z
-      .string()
-      .min(2, { message: "Ingredient list must be at least 2 characters long" })
-      .max(255, {
-        message: "Ingredient list must be less than 255 characters",
-      }),
+    ingredients: z.string().min(2, { message: 'Ingredient list must be at least 2 characters long' }).max(255, {
+      message: 'Ingredient list must be less than 255 characters',
+    }),
     stock: z.number().max(Infinity),
   });
 
   const form = useForm<z.infer<typeof productSchema>>({
     resolver: zodResolver(productSchema),
     defaultValues: {
-      title: "",
-      description: "",
+      title: '',
+      description: '',
       price: 1,
-      image: "",
-      category: "",
-      manufacturer: "",
-      color: "",
-      size: "",
+      image: '',
+      category: '',
+      manufacturer: '',
+      color: '',
+      size: '',
       isarchived: false,
       isfeatured: false,
-      ingredients: "",
+      ingredients: '',
       stock: 0,
     },
   });
 
   function onSubmitting(values: z.infer<typeof productSchema>) {
-    console.log("Onsubmitting:" + values);
+    console.log('Onsubmitting:' + values);
     axios
       .post(`/api/${params.storeID}/products/${params.id}`, values)
       .then(function (response) {
-        toast.success("Product added");
+        toast.success('Product added');
         setInterval(() => {
           window.location.reload();
         }, 3000);
@@ -88,19 +69,14 @@ export default function ProductForm() {
   }
 
   return (
-    <ScrollArea className={"h-[500px] w-[350px] rounded-md border p-8"}>
-      <div className={""}>
-        <h4 className={"mb-2 text-lg font-bold text-center"}>
-          Add new product
-        </h4>
-        <p className={"text-sm text-center text-gray-700 mb-8"}>
+    <ScrollArea className={'h-[700px] w-[400px] rounded-md p-2'}>
+      <div className={'p-2'}>
+        <h4 className={'mb-2 text-lg font-bold text-center'}>Add new product</h4>
+        <p className={'text-sm text-center text-gray-700 mb-8'}>
           Fill in the form to add information about your new product
         </p>
         <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmitting)}
-            className="space-y-8"
-          >
+          <form onSubmit={form.handleSubmit(onSubmitting)} className="space-y-8">
             <FormField
               control={form.control}
               name="title"
@@ -108,10 +84,7 @@ export default function ProductForm() {
                 <FormItem>
                   <FormLabel>Name</FormLabel>
                   <FormControl>
-                    <Input
-                      placeholder="What's the name of your product?"
-                      {...field}
-                    />
+                    <Input placeholder="What's the name of your product?" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -137,12 +110,7 @@ export default function ProductForm() {
                 <FormItem>
                   <FormLabel>Unit Price</FormLabel>
                   <FormControl>
-                    <Input
-                      type="number"
-                      min={1}
-                      {...field}
-                      onChange={(event) => field.onChange(+event.target.value)}
-                    />
+                    <Input type="number" min={1} {...field} onChange={(event) => field.onChange(+event.target.value)} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -179,7 +147,7 @@ export default function ProductForm() {
               <SelectForAddProduct
                 placeholder="Select Color"
                 apicall={`/api/${params.storeID}/colors`}
-                valueSend={(value: string) => form.setValue("color", value)}
+                valueSend={(value: string) => form.setValue('color', value)}
               />
             </FormItem>
             <FormItem>
@@ -187,7 +155,7 @@ export default function ProductForm() {
               <SelectForAddProduct
                 placeholder="Select size"
                 apicall={`/api/${params.storeID}/sizes`}
-                valueSend={(value: string) => form.setValue("size", value)}
+                valueSend={(value: string) => form.setValue('size', value)}
               />
             </FormItem>
             <FormField
@@ -208,16 +176,11 @@ export default function ProductForm() {
               name="isfeatured"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className={"mr-2"}>Featured?</FormLabel>
+                  <FormLabel className={'mr-2'}>Featured?</FormLabel>
                   <FormControl>
-                    <Checkbox
-                      checked={field.value}
-                      onCheckedChange={field.onChange}
-                    />
+                    <Checkbox checked={field.value} onCheckedChange={field.onChange} />
                   </FormControl>
-                  <FormDescription>
-                    If checked, this product will be featured on your web shop.
-                  </FormDescription>
+                  <FormDescription>If checked, this product will be featured on your web shop.</FormDescription>
                   <FormMessage />
                 </FormItem>
               )}
@@ -227,16 +190,11 @@ export default function ProductForm() {
               name="isarchived"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className={"mr-2"}>Archived?</FormLabel>
+                  <FormLabel className={'mr-2'}>Archived?</FormLabel>
                   <FormControl>
-                    <Checkbox
-                      checked={field.value}
-                      onCheckedChange={field.onChange}
-                    />
+                    <Checkbox checked={field.value} onCheckedChange={field.onChange} />
                   </FormControl>
-                  <FormDescription>
-                    If checked, this product will be stored in your archive.
-                  </FormDescription>
+                  <FormDescription>If checked, this product will be stored in your archive.</FormDescription>
                   <FormMessage />
                 </FormItem>
               )}
@@ -248,12 +206,7 @@ export default function ProductForm() {
                 <FormItem>
                   <FormLabel>Product Stock</FormLabel>
                   <FormControl>
-                    <Input
-                      type="number"
-                      min={1}
-                      {...field}
-                      onChange={(event) => field.onChange(+event.target.value)}
-                    />
+                    <Input type="number" min={1} {...field} onChange={(event) => field.onChange(+event.target.value)} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -264,7 +217,7 @@ export default function ProductForm() {
               <SelectForAddProduct
                 placeholder="Select Category"
                 apicall={`/api/${params.storeID}/categories`}
-                valueSend={(value: string) => form.setValue("category", value)}
+                valueSend={(value: string) => form.setValue('category', value)}
               />
             </FormItem>
             <Button type="submit">Submit</Button>
