e984d548e61a3f4a8e4f32b45a8c6a13783a0059
Shinyn <mattias.alm@hotmail.com>
2023-10-19 05:30:26 +0200

    updated styling

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,77 +1,67 @@
 //En egengjord navbar som löper längs med sidan
-"use client";
-import Link from "next/link";
-import { Settings } from "@/components/stores/dashboard/settings";
-import { useParams } from "next/navigation";
-import { MdArrowBackIos } from "react-icons/md";
+'use client';
+import Link from 'next/link';
+import { Settings } from '@/components/stores/dashboard/settings';
+import { useParams } from 'next/navigation';
+import { MdArrowBackIos } from 'react-icons/md';
+import { AiFillHome } from 'react-icons/ai';
 
 export default function Navbar() {
   const params = useParams();
 
   return (
-    <div
-      className={"sticky top-0 h-screen bg-blue-400 border-r-2 print:hidden "}
-    >
-      <div className={"flex flex-row justify-start"}>
-        <Settings />
+    <div className={'sticky top-0 h-screen bg-blue-400 print:hidden '}>
+      <div className="flex flex-row justify-left m-4 text-4xl">
+        <Link href={`/`}>
+          <AiFillHome className=" hover:text-white mr-4 rounded-full border border-slate p-1" />
+        </Link>
+        <div className={'flex flex-row justify-start'}>
+          <Settings />
+        </div>
       </div>
       <div>
-        <h1
-          className={
-            " my-6 text-4xl hover:text-white hover:bg-blue-500 px-4 rounded py-1"
-          }
-        >
-          {" "}
-          <Link href={`/${params.storeID}`}>Dashboard</Link>
+        <h1 className={'my-6 text-4xl hover:text-white hover:bg-blue-500 rounded py-1'}>
+          <Link className="flex-1 px-4 py-2" href={`/${params.storeID}`}>
+            Dashboard
+          </Link>
         </h1>
-        <ul className={"flex-col space-y-6 text-2xl"}>
-          <li
-            className={"hover:text-white hover:bg-blue-500 px-4 py-1 rounded"}
-          >
-            <Link href={`/${params.storeID}/billboards`}> Banner</Link>
+        <ul className={'flex-col text-2xl'}>
+          <li className={'hover:text-white hover:bg-blue-500 flex rounded'}>
+            <Link className="flex-1 px-4 py-2" href={`/${params.storeID}/billboards`}>
+              Billboard
+            </Link>
           </li>
-          <li
-            className={"hover:text-white hover:bg-blue-500 px-4 py-1 rounded"}
-          >
-            <Link href={`/${params.storeID}/products`}>Products</Link>
+          <li className={'hover:text-white hover:bg-blue-500 flex rounded'}>
+            <Link className="flex-1 px-4 py-2" href={`/${params.storeID}/products`}>
+              Products
+            </Link>
           </li>
-          <li
-            className={
-              "m-1 hover:text-white hover:bg-blue-500 px-4 py-1 rounded text-xl"
-            }
-          >
-            <Link href={`/${params.storeID}/categories`}>- Categories</Link>
+          <li className={'hover:text-white hover:bg-blue-500 flex rounded text-xl'}>
+            <Link className="flex-1 px-4 py-2" href={`/${params.storeID}/categories`}>
+              - Categories
+            </Link>
           </li>
-          <li
-            className={
-              "m-1 hover:text-white hover:bg-blue-500 px-4 py-1 rounded text-xl"
-            }
-          >
-            <Link href={`/${params.storeID}/sizes`}>- Sizes</Link>
+          <li className={'hover:text-white hover:bg-blue-500 flex rounded text-xl'}>
+            <Link className="flex-1 px-4 py-2" href={`/${params.storeID}/sizes`}>
+              - Sizes
+            </Link>
           </li>
-          <li
-            className={
-              "m-1 hover:text-white hover:bg-blue-500 px-4 py-1 rounded text-xl"
-            }
-          >
-            <Link href={`/${params.storeID}/colors`}>- Colors</Link>
+          <li className={'hover:text-white hover:bg-blue-500 flex rounded text-xl'}>
+            <Link className="flex-1 px-4 py-2" href={`/${params.storeID}/colors`}>
+              - Colors
+            </Link>
           </li>
-          <li
-            className={"hover:text-white hover:bg-blue-500 px-4 py-1 rounded"}
-          >
-            <Link href={`/${params.storeID}/customers`}>Customers</Link>
+          <li className={'hover:text-white hover:bg-blue-500 flex rounded'}>
+            <Link className="flex-1 px-4 py-2" href={`/${params.storeID}/customers`}>
+              Customers
+            </Link>
           </li>
-          <li
-            className={"hover:text-white hover:bg-blue-500 px-4 py-1 rounded"}
-          >
-            <Link href={`/${params.storeID}/orders`}>Orders</Link>
+          <li className={'hover:text-white hover:bg-blue-500 flex rounded'}>
+            <Link className="flex-1 px-4 py-2" href={`/${params.storeID}/orders`}>
+              Orders
+            </Link>
           </li>
         </ul>
-      </div>
-      <div className="flex flex-row justify-end py-1 mt-20 mr-6">
-        <Link href={`/`}>
-          <MdArrowBackIos className="text-4xl hover:text-white" />
-        </Link>
       </div>
     </div>
   );
