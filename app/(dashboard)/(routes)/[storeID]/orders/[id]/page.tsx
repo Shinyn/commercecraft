69cd995421b9256f637c6f4dc584a23ca5cd86dc
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-10-19 10:24:47 +0200

    printable

--- app/(dashboard)/(routes)/[storeID]/orders/[id]/page.tsx
+++ app/(dashboard)/(routes)/[storeID]/orders/[id]/page.tsx
@@ -1,9 +1,9 @@
-'use client';
-import React from 'react';
-import { useParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Order } from '@/components/stores/dashboard/orders/order';
+"use client";
+import React from "react";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Order } from "@/components/stores/dashboard/orders/order";
 
 const OrderPage = () => {
   const params = useParams();
@@ -15,8 +15,12 @@ const OrderPage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const ordersResponse = await axios.get(`/api/${params.storeID}/orders/${params.id}`);
-        const customersResponse = await axios.get(`/api/${params.storeID}/customers/${ordersResponse.data.customerId}`);
+        const ordersResponse = await axios.get(
+          `/api/${params.storeID}/orders/${params.id}`
+        );
+        const customersResponse = await axios.get(
+          `/api/${params.storeID}/customers/${ordersResponse.data.customerId}`
+        );
         const orderItemsResponse = await axios.get(
           `/api/${params.storeID}/order_items/${ordersResponse.data.order_number}`
         );
@@ -45,11 +49,12 @@ const OrderPage = () => {
     );
   }
   function CustomersDiv({ customers }: any) {
-    const { firstName, lastName, e_mail, phone, street, city, zipCode } = customers;
+    const { firstName, lastName, e_mail, phone, street, city, zipCode } =
+      customers;
     return (
       <div className="">
         <h1 className="text-xl">Customer Information:</h1>
-        <p className="font-bold">{firstName + ' ' + lastName}</p>
+        <p className="font-bold">{firstName + " " + lastName}</p>
         <p className="">{e_mail}</p>
         <p className=""> {phone}</p>
         <p className="font-bold">Adress:</p>
@@ -64,7 +69,11 @@ const OrderPage = () => {
   function BottomDiv({ orders }: any) {
     const { order_total } = orders;
     return (
-      <div className={'flex flex-row justify-between border-t border-black absolute bottom-0 w-full'}>
+      <div
+        className={
+          "flex flex-row justify-between border-t border-black absolute bottom-0 w-full"
+        }
+      >
         <div>
           <h1 className="text-2xl font-bold m-1">Pris:</h1>
           <span className="font-bold m-1">Moms:</span>{" "}
@@ -73,7 +82,6 @@ const OrderPage = () => {
             <span className="font-bold m-1">Total:</span>
             <span>{order_total} SEK</span>
           </div>
-
           <p className="italic pl-1"> Varav Frakt: 50kr </p>
         </div>
       </div>
@@ -81,16 +89,16 @@ const OrderPage = () => {
   }
 
   return (
-    <div className="relative h-screen w-[1400px] border border-black">
+    <div className="relative h-screen w-[1000px] border border-x-2 border-black">
       <div>
         <div className="border-b border-black flex flex-row justify-around p-9">
           {loaded ? (
             <>
               <OrdersDiv orders={orders} />
-              <CustomersDiv customers={customers} />{' '}
+              <CustomersDiv customers={customers} />{" "}
             </>
           ) : (
-            'Loading'
+            "Loading"
           )}
         </div>
         <div className="border-black  ">
@@ -109,7 +117,9 @@ const OrderPage = () => {
                     {order_items.map((item: any, index) => (
                       <tr
                         key={item.id}
-                        className={`border-b border-black ${index % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200'}`}
+                        className={`border-b border-black ${
+                          index % 2 === 0 ? "bg-gray-100" : "bg-gray-200"
+                        }`}
                       >
                         <td className="py-2 px-4">{item.title}</td>
                         <td className="py-2 px-4">x{item.amount}</td>
@@ -120,7 +130,7 @@ const OrderPage = () => {
                 </table>
               </>
             ) : (
-              'Loading'
+              "Loading"
             )}
           </div>
         </div>
