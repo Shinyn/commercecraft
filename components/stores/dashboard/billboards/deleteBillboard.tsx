81542f5238a1ed785eb14cb0588d1bf0abd523f9
BeccaFi <rebecca.fischer.dev@gmail.com>
2023-10-19 12:39:12 +0200

    Changed component name

--- components/stores/dashboard/billboards/deleteBillboard.tsx
+++ components/stores/dashboard/billboards/deleteBillboard.tsx
@@ -1,27 +1,33 @@
-import axios, { isAxiosError } from "axios"
+import axios, { isAxiosError } from "axios";
 import toast from "react-hot-toast";
 import { useBillboards } from "@/components/stores/dashboard/billboards/zustand/zustandstate";
 import { useParams } from "next/navigation";
 
-export function MyDelete(params: { itemId: string }) {
+export function DeleteBillboard(params: { itemId: string }) {
   const reFetchBillboards = useBillboards((state) => state.reFetchBillboards);
-  const { storeID } = useParams()
+  const { storeID } = useParams();
   return (
-    <div onClick={async (e) => {
-      axios.delete(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/${storeID}/billboards/${params.itemId}`)
-        .then(function (response) {
-          toast.success("Billboard deleted successfully");
-          reFetchBillboards(Array.isArray(storeID) ? storeID.toString() : storeID)
-          return response
-        })
-        .catch(function (error) {
-          if (isAxiosError(error)) {
-            return error.message;
-          }
-        });
-    }
-    }>
+    <div
+      onClick={async (e) => {
+        axios
+          .delete(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/${storeID}/billboards/${params.itemId}`
+          )
+          .then(function (response) {
+            toast.success("Billboard deleted successfully");
+            reFetchBillboards(
+              Array.isArray(storeID) ? storeID.toString() : storeID
+            );
+            return response;
+          })
+          .catch(function (error) {
+            if (isAxiosError(error)) {
+              return error.message;
+            }
+          });
+      }}
+    >
       Delete
     </div>
-  )
+  );
 }
