7b34e58f130fb9b17048912fe0a8808fb618433d
TereseBo <terese.boderus@gmail.com>
2023-10-19 16:10:13 +0200

    Locked checkboxes in table

--- components/stores/dashboard/billboards/columns.tsx
+++ components/stores/dashboard/billboards/columns.tsx
@@ -64,7 +64,8 @@ export const columns: ColumnDef<Billboard>[] = [
     },
     cell: ({ row }) => (
       <Checkbox
-        checked={row.original.active==1?true:false}
+        aria-readonly={true}
+        checked={row.original.active == 1 ? true : false}
         aria-label="Select row"
       />)
   },
