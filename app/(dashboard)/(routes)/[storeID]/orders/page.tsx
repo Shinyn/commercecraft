302134e46390ffa57c0ebc0d70127b7298eec169
Shinyn <mattias.alm@hotmail.com>
2023-10-19 05:27:19 +0200

    prettier mmkay?

--- app/(dashboard)/(routes)/[storeID]/orders/page.tsx
+++ app/(dashboard)/(routes)/[storeID]/orders/page.tsx
@@ -1,24 +1,24 @@
-"use client";
-import { useEffect} from "react";
-import { useParams } from "next/navigation";
-import { DataTable } from "@/components/data-table";
-import { columns } from "@/components/stores/dashboard/orders/columns";
-import { useOrders } from "@/components/stores/dashboard/orders/zustand/ordersState";
-import { APIList } from "@/components/stores/dashboard/api-list/APIList";
+'use client';
+import { useEffect } from 'react';
+import { useParams } from 'next/navigation';
+import { DataTable } from '@/components/data-table';
+import { columns } from '@/components/stores/dashboard/orders/columns';
+import { useOrders } from '@/components/stores/dashboard/orders/zustand/ordersState';
+import { APIList } from '@/components/stores/dashboard/api-list/APIList';
 
 export default function Page() {
   //states for keeping track of billboards in dashboard front
-  const{storeID}=useParams()
+  const { storeID } = useParams();
   const orders = useOrders((state) => state.orders);
-  const section = "orders";
+  const section = 'orders';
   const reFetchCompleteOrders = useOrders((state) => state.reFetchCompleteOrders);
 
   useEffect(() => {
-    reFetchCompleteOrders(Array.isArray(storeID) ? storeID.toString() : storeID)
+    reFetchCompleteOrders(Array.isArray(storeID) ? storeID.toString() : storeID);
   }, []);
 
   return (
-    <div className={"flex flex-col"}>
+    <div className={'flex flex-col'}>
       <div>
         <div className="container mx-auto py-10">
           <DataTable columns={columns} data={orders} />
