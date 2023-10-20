b6cc4f6904c2ef912ff447db788b8ee8c351136a
BeccaFi <rebecca.fischer.dev@gmail.com>
2023-10-20 20:43:16 +0200

    Updated params

--- components/stores/dashboard/colors/DeleteColor.tsx
+++ components/stores/dashboard/colors/DeleteColor.tsx
@@ -3,7 +3,7 @@ import { useParams } from 'next/navigation';
 import toast from 'react-hot-toast';
 import { useColors } from '@/components/stores/dashboard/colors/zustand/zustandstate';
 
-export function DeleteColor(params: { itemId: string }) {
+export function DeleteColor(params: { itemName: string }) {
   const { storeID } = useParams();
   const reFetchColors = useColors((state) => state.reFetchColors);
 
@@ -12,10 +12,14 @@ export function DeleteColor(params: { itemId: string }) {
       className="hover:cursor-pointer w-full p-2"
       onClick={async (e) => {
         axios
-          .delete(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/${storeID}/colors/${params.itemId}`)
+          .delete(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/${storeID}/colors/${params.itemName}`
+          )
           .then(function (response) {
             toast.success('Color deleted successfully');
-            reFetchColors(Array.isArray(storeID) ? storeID.toString() : storeID);
+            reFetchColors(
+              Array.isArray(storeID) ? storeID.toString() : storeID
+            );
             return response.data;
           })
           .catch(function (error) {
--- components/stores/dashboard/sizes/DeleteSize.tsx
+++ components/stores/dashboard/sizes/DeleteSize.tsx
@@ -2,14 +2,16 @@ import axios from 'axios';
 import { useParams } from 'next/navigation';
 import toast from 'react-hot-toast';
 
-export function DeleteSize(params: { itemId: string }) {
+export function DeleteSize(params: { itemName: string }) {
   const { storeID } = useParams();
   return (
     <div
       className="hover:cursor-pointer w-full p-2"
       onClick={async (e) => {
         axios
-          .delete(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/${storeID}/sizes/${params.itemId}`)
+          .delete(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/${storeID}/sizes/${params.itemName}`
+          )
           .then(function (response) {
             toast.success('Size deleted successfully');
             window.location.reload();
