c281288166a4dae3bd8862ac22eef909be0fb7fa
arapg <hello@arpg.dev>
2023-10-20 14:17:05 +0200

    added GET endpoint at /orders

---
+++ app/api/[storeID]/orders/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import prismadb from "@/lib/db";
+
+export async function GET(
+    req: Request,
+    { params }: { params: { storeID: string; id: string } }
+  ) {
+    const storeId = params.storeID;
+    const id = params.id;
+    if (id) {
+      try {
+        const Order = await prismadb.order.findUnique({
+          where: { id },
+        });
+        return NextResponse.json(Order);
+      } catch (error) {
+        console.log("api/[storeId]/orders/[id]/GET", error);
+        return new NextResponse(
+          "Ooops, something went wrong when getting the customer",
+          { status: 500 }
+        );
+      }
+    } else {
+      try {
+        const Orders = await prismadb.order.findMany({
+          where: { storeId },
+        });
+        return NextResponse.json(Orders);
+      } catch (error) {
+        console.log("api/[storeId]/customers/all/GET", error);
+        return new NextResponse(
+          "Ooops, something went wrong when getting the customerlist",
+          { status: 500 }
+        );
+      }
+    }
+  }
