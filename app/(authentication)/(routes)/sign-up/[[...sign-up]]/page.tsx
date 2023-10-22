88854479c4d6045e7e92a7076c35326129a2e4ba
arapg <hello@arpg.dev>
2023-10-22 19:48:13 +0200

    centered both signup and signin component

--- app/(authentication)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(authentication)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -3,5 +3,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className='grid h-screen w-screen place-items-center'>
+      <SignUp />
+    </div>
+  );
 }
