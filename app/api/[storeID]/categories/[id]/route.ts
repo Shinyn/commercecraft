542478997356abdd9948731b4f77d46158488bf7
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-10-23 15:13:03 +0200

    does it work now?

--- app/api/[storeID]/categories/[id]/route.ts
+++ app/api/[storeID]/categories/[id]/route.ts
@@ -1,25 +1,25 @@
 //File contains DELETE for category by ID
 
-import { NextResponse } from 'next/server';
-import prismadb from '@/lib/db';
+import { NextResponse } from "next/server";
+import prismadb from "@/lib/db";
 
 export async function DELETE(
   req: Request,
-  { params }: { params: { storeID:string, id: string } }
+  { params }: { params: { storeID: string; id: string } }
 ) {
   try {
     const title = params.id;
-    const storeId=params.storeID;
+    const storeId = params.storeID;
     const deletedCategory = prismadb.category.deleteMany({
       where: {
         title,
-        storeId
+        storeId,
       },
     });
     const removeFromProduct = prismadb.product.updateMany({
-      where: { category: title, storeId },
+      where: { category: title, storeId: storeId },
       data: {
-        category: '-',
+        category: "-",
       },
     });
 
@@ -30,9 +30,9 @@ export async function DELETE(
 
     return NextResponse.json(deletedCategory, { status: 200 });
   } catch (error) {
-    console.log('api/categories/DELETE', error);
+    console.log("api/categories/DELETE", error);
     return new NextResponse(
-      'Ooops, something went wrong when deleting the category',
+      "Ooops, something went wrong when deleting the category",
       { status: 500 }
     );
   }
